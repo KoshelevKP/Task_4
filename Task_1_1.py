bc712f48a6bd544108b383b63081c3cce8b85e3a
KoshelevKP <43110471+KoshelevKP@users.noreply.github.com>
2018-09-18 11:43:38 +0300

    Add files via upload

---
+++ Task_1_1.py
@@ -0,0 +1,22 @@
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+directory = os.path.join(current_dir, 'Migrations')
+
+files = os.listdir(directory)
+sql_files = list()
+for file in files:
+    if 'sql' in file[-3:]:
+        sql_files.append(file)
+
+while True:
+    if len(sql_files)>0:
+        word = input("введите строку ")
+        for file in sql_files:
+            with open(os.path.join(directory,file)) as f:
+                if word not in f.read().split(" "):
+                    sql_files.remove(file)
+        for file in sql_files:
+            print(file)
+        print(len(sql_files))
+            
